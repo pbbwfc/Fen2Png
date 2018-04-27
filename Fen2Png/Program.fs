45ad5cd75a53755573c56e7489378cf931264e1e
Phil Brooks <phil.brooks@asr-services.com>
2018-04-27 10:16:02 +0100

    Completed to support arguments

--- Fen2Png/Program.fs
+++ Fen2Png/Program.fs
@@ -1,18 +1,28 @@
-﻿open System.Drawing
-open System.Drawing.Imaging
+﻿open System
 open System.IO
 open Fen2Png
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    if argv.Length<>2 then
+        Console.WriteLine "Need to provide 2 arguments: 1. Folder containing epd file(s) 2. b/w specifying needing diagram from Black's or White's view."
+        1
+    else
+        let ifol = argv.[0]
+        let flip = argv.[1]="b"
+        let fol = if ifol.[1]=':' then ifol else Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), ifol)
+        if not (Directory.Exists fol) then
+            Console.WriteLine("Invalid folder supplied: " + fol)
+            1
+        else
+            Console.WriteLine("Using folder: " + fol)
+            if flip then Console.WriteLine("Diagrams are from Black's view") else Console.WriteLine("Diagrams are from White's view")
+            let epds = Directory.GetFiles(fol,"*.epd")|>Array.map(fun e -> new Epd(e))
+            if epds.Length=0 then 
+                Console.WriteLine("No EPD files found in folder: " + fol)
+                1
+            else
+                epds|>Array.iter(fun e -> e.ToPngs(flip))
+                Console.WriteLine("Finished creating PNG diagrams in folder: " + fol)
 
-    let p1 = Pos.FromString("rnbqkbnr/ppp2ppp/4p3/8/2p5/6P1/PP1PPPBP/RNBQK1NR w KQkq - 0 4")
-    p1.ToPng("1_1_1.png",true)
-
-    let wrkfol = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "wrk")
-    let epds = Directory.GetFiles(wrkfol,"*.epd")|>Array.map(fun e -> new Epd(e))
-    epds|>Array.iter(fun e -> e.ToPngs(true))
-
-
-    0 // return an integer exit code
+                0 // return an integer exit code
